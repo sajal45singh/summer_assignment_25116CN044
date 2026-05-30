40466fecf3fa92b33431ab94e541328220681296
sajal45singh <sajalsingh216@gmail.com>
2026-05-30 23:09:38 +0530

    Add files via upload

---
+++ Day3/question2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n) {
+    if (n < 2)
+        return false;
+
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0)
+            return false;
+    }
+
+    return true;
+}
+
+int main() {
+    int start, end;
+    
+    cout << "Enter the range: ";
+    cin >> start >> end;
+
+    cout << "Prime numbers between " << start << " and " << end << " are:\n";
+
+    for (int i = start; i <= end; i++) {
+        if (isPrime(i))
+            cout << i << " ";
+    }
+
+    return 0;
+}
---
+++ Day3/question3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10, b = 20;
+
+    cout << "First number: " << a << endl;
+    cout << "Second number: " << b << endl;
+
+    return 0;
+}    
